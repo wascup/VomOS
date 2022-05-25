d243e3de56ce193ba12629e21b04f221fdd41350
Jacob Bochmann <72769017+wascup@users.noreply.github.com>
2022-05-24 20:08:40 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # VomOS
  An OS for my car
+ Well I guess its more of an interface, who cares.
---
+++ src/VOM/UIHandler.java
@@ -0,0 +1,13 @@
+package VOM;
+
+import javax.swing.*;
+
+public class UIHandler extends JFrame {
+    boolean debug = true;
+    JButton gpsButton = new JButton("GPS");
+    JButton mediaButton = new JButton("Media");
+
+    public UIHandler() {
+
+    }
+}
